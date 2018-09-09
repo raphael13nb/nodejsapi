8f8748330f00b7dcec49857e6c4ee86a1cb8dc8d
Raphael bressam <raphaelwd@hotmail.com>
2018-09-08 23:27:32 -0300

    connection

--- ormConnection.js
+++ ormConnection.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('ApiTest', 'sa', 'genesysjp', {
-    host: 'localhost',
-    port: 50000,
+const sequelize = new Sequelize('ApiTest', 'sa', '!InfinitSolucoes11', {
+    host: 'infinitvmlinuxdb.brazilsouth.cloudapp.azure.com',
+    port: 1433,
     dialect: 'mssql',
     freezeTableName: true,
     operatorsAliases: false,
